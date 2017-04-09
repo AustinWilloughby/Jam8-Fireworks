d922f86d6eb6e0b1d0f510f4c896637bc423f790
Austin Willoughby <arw8020@rit.edu>
2017-04-09 13:12:20 -0400

    Fireworks Implemented
    Implemented the firework system

---
+++ Fireworks/Assets/Firework.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firework : MonoBehaviour
+{
+    //Fields
+    //Public
+    public ParticleSystem explosionSystem;
+    public Rigidbody2D rigid;
+    public FireworkManager manager;
+    public Vector2 launchPoint;
+
+    //Private
+    private bool isFlying = false;
+    private float resetTimer;
+    private bool isTarget = false;
+
+
+    //Properties
+    public bool IsFlying
+    {
+        get { return isFlying; }
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+        ResetFirework();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFlying)
+        {
+            if (rigid.velocity.magnitude > 0)
+            {
+                transform.rotation = Quaternion.Euler(0, 0, Mathf.Rad2Deg * Mathf.Atan(rigid.velocity.y / rigid.velocity.x) - 90);
+            }
+            resetTimer -= Time.deltaTime;
+            if (resetTimer <= 0)
+            {
+                manager.CheckForNoExplosion();
+                ResetFirework();
+                
+            }
+        }
+    }
+
+    public void Launch(Vector2 launchVelocity, bool target)
+    {
+        transform.position = launchPoint;
+        rigid.simulated = true;
+        rigid.AddForce(launchVelocity);
+        isFlying = true;
+        isTarget = target;
+    }
+
+    private void OnMouseDown()
+    {
+        if (manager.HandleExplosion(this))
+        {
+            ResetFirework();
+        }
+    }
+
+    private void ResetFirework()
+    {
+        isFlying = false;
+        rigid.simulated = false;
+        resetTimer = manager.timeBetweenLaunches - 4.0f;
+        manager.CheckIfFireworksFlying();
+        rigid.velocity = Vector2.zero;
+        transform.position = manager.transform.position;
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+
+}
---
+++ Fireworks/Assets/FireworkManager.cs
@@ -0,0 +1,205 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class FireworkManager : MonoBehaviour
+{
+    //Fields
+    //Public
+    public int numFireworks = 3;
+    public GameObject fireworkPrefab;
+    public ParticleSystem explosion;
+    public float timeBetweenLaunches = 10.0f;
+    public Vector2 launchVelocity;
+    
+    //Private
+    private List<Firework> fireworks;
+    private bool fireworksLaunched = false;
+    private bool clickedRightFirework = false;
+    private bool alreadyExploded = false;
+    private bool alreadyCalled = false;
+    private float launchTimer = 0;
+    private FireworkColors nextColorCalled;
+    private FireworkColors lastColorCalled;
+    
+    // Use this for initialization
+    void Start()
+    {
+        Vector2 almostTopLeft = GameObject.Find("Main Camera").GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0, 1.0f));
+        Vector2 almostBottomLeft = GameObject.Find("Main Camera").GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0, .3f));
+
+        fireworks = new List<Firework>();
+
+        for(int i = 0; i < numFireworks; i++)
+        {
+            GameObject obj = Instantiate(fireworkPrefab);
+            obj.transform.position = transform.position;
+            obj.transform.parent = transform;
+            fireworks.Add(obj.GetComponent<Firework>());
+            fireworks[i].manager = this;
+            fireworks[i].explosionSystem = explosion;
+            fireworks[i].launchPoint = new Vector2(almostTopLeft.x - fireworkPrefab.transform.localScale.x * 2, Mathf.Lerp(almostTopLeft.y, almostBottomLeft.y, ((float)i + 1) / numFireworks));
+        }
+        launchTimer = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        launchTimer -= Time.deltaTime;
+        if(launchTimer <= 0 && alreadyCalled == false)
+        {
+            alreadyCalled = true;
+            nextColorCalled = GetRandomFireworkColor();
+            //RUCHITA - Trigger the audience yelling the color name here. Use nextColorCalled as the color that they yell out.
+            //There is a method called GetStringFromEnumValue() below that will return the string of the color.
+            //Just call it like: GetStringFromEnumValue(nextColorCalled), and use the string it returns to have the crowd shout.
+            //There is also a method called GetColorFromEnumValue() which you can also pass nextColorCalled to get the Color object back
+            //to use when you have the text draw. If you have any questions just message me @haroldthehobo in the discord channel.
+            Debug.Log("We want " + GetStringFromEnumValue(nextColorCalled) + "!");
+        }
+        if (launchTimer <= -3.0f && fireworksLaunched == false)
+        {
+            LaunchFireworkSet(nextColorCalled);
+            launchTimer = timeBetweenLaunches;
+        }
+    }
+
+    public void LaunchFireworkSet(FireworkColors neededColor)
+    {
+        lastColorCalled = neededColor;
+        alreadyExploded = false;
+        alreadyCalled = false;
+        if (!CheckIfFireworksFlying())
+        {
+            var values = System.Enum.GetValues(typeof(FireworkColors));
+            List<FireworkColors> colors = values.Cast<FireworkColors>().Where(val => val != neededColor).ToList();
+            int targetIndex = Random.Range(0, fireworks.Count);
+            for(int i = 0; i < fireworks.Count; i++)
+            {
+                if(i == targetIndex)
+                {
+                    fireworks[i].Launch(launchVelocity, true);
+                    fireworks[i].gameObject.GetComponent<SpriteRenderer>().color = GetColorFromEnumValue(neededColor);
+                }
+                else
+                {
+                    FireworkColors unique = colors[Random.Range(0, colors.Count)];
+                    colors.Remove(unique);
+                    fireworks[i].Launch(launchVelocity, false);
+                    fireworks[i].gameObject.GetComponent<SpriteRenderer>().color = GetColorFromEnumValue(unique);
+                }
+            }
+        }
+    }
+
+    public bool HandleExplosion(Firework fw)
+    {
+        if (!alreadyExploded)
+        {
+            explosion.transform.position = fw.transform.position;
+            explosion.startColor = fw.gameObject.GetComponent<SpriteRenderer>().color;
+            explosion.Play();
+            alreadyExploded = true;
+
+            if(explosion.startColor == GetColorFromEnumValue(lastColorCalled))
+            {
+                //RUCHITA - Handle crowd cheering here, player clicked the right color
+                Debug.Log("YAY!!!");
+            }
+            else
+            {
+                //RUCHITA - Handle crowd booing here, player clicked the wrong color
+                Debug.Log("BOO!!!");
+            }
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+    //HELPER FUNCTIONS
+    public bool CheckIfFireworksFlying()
+    {
+        bool launched = false;
+        foreach (Firework work in fireworks) //Ensure all the fireworks have returned.
+        {
+            if (work.IsFlying) { launched = true; }
+        }
+        fireworksLaunched = launched;
+        return fireworksLaunched;
+    }
+
+    public void CheckForNoExplosion()
+    {
+        if(fireworksLaunched && !alreadyExploded)
+        {
+            //RUCHITA - Handle crowd booing here, player didn't click a firework
+            Debug.Log("BOO!!!");
+            alreadyExploded = true;
+        }
+    }
+
+    public Color GetColorFromEnumValue(FireworkColors col)
+    {
+        switch (col)
+        {
+            case FireworkColors.Red:
+                return Color.red;
+            case FireworkColors.Green:
+                return Color.green;
+            case FireworkColors.Blue:
+                return Color.cyan;
+            case FireworkColors.Purple:
+                return Color.magenta;
+            case FireworkColors.White:
+                return Color.white;
+            case FireworkColors.Yellow:
+                return Color.yellow;
+            default:
+                return Color.black;
+        }
+    }
+
+    public string GetStringFromEnumValue(FireworkColors col)
+    {
+        switch (col)
+        {
+            case FireworkColors.Red:
+                return "RED";
+            case FireworkColors.Green:
+                return "GREEN";
+            case FireworkColors.Blue:
+                return "BLUE";
+            case FireworkColors.Purple:
+                return "PURPLE";
+            case FireworkColors.White:
+                return "WHITE";
+            case FireworkColors.Yellow:
+                return "YELLOW";
+            default:
+                return "BLACK";
+        }
+    }
+
+    public FireworkColors GetRandomFireworkColor()
+    {
+        var values = System.Enum.GetValues(typeof(FireworkColors));
+        return (FireworkColors)values.GetValue(Random.Range(0, values.Length));
+    }
+}
+
+public enum FireworkColors
+{
+    Red,
+    Green,
+    Blue,
+    Purple,
+    White,
+    Yellow
+};
